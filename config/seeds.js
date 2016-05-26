3cc6e5e8b8508951ea20489d0649ff9c3f6f7905
Jonathan Dai <jonathanjddai@gmail.com>
2016-05-26 14:16:15 -0700

    styling

--- config/seeds.js
+++ config/seeds.js
@@ -12,7 +12,7 @@ var meals = [
       protein: 30,
       fats: 15,
       carbs: 45,
-      day: "Tuesday"
+      day: "Monday"
     },
     {
       title: "Steak & String Beans",
@@ -21,20 +21,47 @@ var meals = [
       protein: 35,
       fats: 20,
       carbs: 35,
-      day: "Monday"
+      day: "Tuesday"
     },
     {
       title: "Salmon & Brocolli",
-      body: "Grilled chicken with steamed brocolli and a side of rice",
-      cals: 500,
-      protein: 30,
-      fats: 15,
+      body: "Baked salmon with steamed brocolli and a side of rice",
+      cals: 450,
+      protein: 28,
+      fats: 20,
       carbs: 45,
       day: "Wednesday"
     },
     {
       title: "Frog & Brocolli",
-      body: "Grilled chicken with steamed brocolli and a side of rice",
+      body: "Cooked frog with steamed brocolli and a side of rice",
+      cals: 300,
+      protein: 40,
+      fats: 13,
+      carbs: 50,
+      day: "Thursday"
+    },
+    {
+      title: "Subway",
+      body: "A subway sandwich, also known as the GA diet",
+      cals: 1000,
+      protein: 20,
+      fats: 30,
+      carbs: 50,
+      day: "Friday"
+    },
+    {
+      title: "Frog & Brocolli",
+      body: "Cooked frog with steamed brocolli and a side of rice",
+      cals: 500,
+      protein: 30,
+      fats: 15,
+      carbs: 45,
+      day: "Saturday"
+    },
+    {
+      title: "Frog & Brocolli",
+      body: "Cooked frog with steamed brocolli and a side of rice",
       cals: 500,
       protein: 30,
       fats: 15,
